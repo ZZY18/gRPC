fb16fbf1ced80f9b36bc59cea7aa2e14a96f8ebc
S20181248@xs.ustb.edu.cn <S20181248@xs.ustb.edu.cn>
2019-04-22 14:39:52 +0800

    对于gRPC基本操作

---
+++ gRPCDemo/client/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Google.Protobuf;
+using GRPCDemo;
+using gRPCDemo;
+
+
+namespace client
+{
+    
+    class newproto : gRPC.gRPCBase
+    
+    {
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply { Message = "Hello " + request.Name });
+        }
+
+        public override Task<list1> Getlsit(HelloRequest request, ServerCallContext context)
+        {
+            ByteString bytestring1 = ByteString.CopyFrom("123",Encoding.Default);
+            return Task.FromResult(new list1 { Listbyte=bytestring1 });
+        }
+    }
+    class Program
+    {
+        const int port = 12321;
+        static void Main(string[] args)
+        {
+            Server server = new Server {
+                Services = { gRPC.BindService(new newproto()) },
+                Ports = { new ServerPort("localhost", port, ServerCredentials.Insecure) }
+            };
+            Console.WriteLine("进入服务");
+            server.Start();
+
+            Console.WriteLine("gRPC server listening on port " + port);
+            Console.WriteLine("任意键退出...");
+            Console.ReadKey();
+
+            server.ShutdownAsync().Wait();
+
+        }
+    }
+}
---
+++ gRPCDemo/server/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using gRPCDemo;
+using Grpc.Core;
+using GRPCDemo;
+using Google.Protobuf;
+
+namespace server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Channel channle = new Channel("127.0.0.1:12321", ChannelCredentials.Insecure);
+            var client = new gRPC.gRPCClient(channle);
+            var result = client.SayHello(new HelloRequest { Name = "niming" }) as HelloReply;
+            var result1 = client.Getlsit(new HelloRequest { Name="123456"}) as list1;
+            string a = result1.Listbyte.ToString(Encoding.Default);
+            Console.WriteLine(a);
+
+            channle.ShutdownAsync().Wait();
+            Console.WriteLine("任意键退出...");
+            Console.ReadKey();
+        }
+    }
+}
